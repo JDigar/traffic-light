3f38c960235ed539b9d3c3463f73636508b408ca
JDigar <jdigar96@gmail.com>
2022-05-30 21:12:01 +0000

    no funciona

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,47 @@
-import React from "react";
+// import { useState } from "react/cjs/react.production.min";
+import { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+// const [color, setColor] = useState("btn-success");
+//
+
+// function iluminar() {}
 
 //create your first component
 const Home = () => {
+	const [color, setColor] = useState(false);
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<>
+			<div className="container text-center">
+				<div>
+					<button
+						type="button"
+						className={
+							"btn btn-success rounded-circle" +
+							(color === true ? "iluminado" : " ")
+						}
+						onClick={() => setColor(false)}></button>
+				</div>
+				<div>
+					<button
+						type="button"
+						class={
+							"btn btn-success rounded-circle" +
+							(color === true ? "iluminado" : " ")
+						}
+						onClick={() => setColor(false)}></button>
+				</div>
+				<div>
+					<button
+						type="button"
+						class={
+							"btn btn-success rounded-circle" +
+							(color === true ? "iluminado" : " ")
+						}
+						onClick={() => setColor(false)}></button>
+				</div>
+			</div>
+		</>
 	);
 };
 
---
+++ src/js/component/trafficLight.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const trafficLight = () => {
+	<button type="button" class="btn btn-success">
+		Success
+	</button>;
+};
+
+export default trafficLight;
